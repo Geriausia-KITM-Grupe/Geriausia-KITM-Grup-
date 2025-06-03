742104a8343dd6936b79d8afc49a365f92a014a4
wdallo <web.dalyzz@gmail.com>
2025-06-03 20:09:33 +0300

    Added Connection to DataBase MangoDB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const connection = require("./config/db.js");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,6 +7,7 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+connection();
 
 app.listen(process.env.PORT, () => {
   console.log("Server up and running on port ===> " + process.env.PORT);
