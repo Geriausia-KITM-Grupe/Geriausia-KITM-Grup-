f27fb6ccd901011ad1c7d925299f456da5aa1103
wdallo <web.dalyzz@gmail.com>
2025-06-07 16:35:26 +0300

    Added Error Page 404, Added Same Code to SearchHeader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+
 import Slider from "./components/silder/Slider";
 import EventList from "./pages/EventList";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
       />
       <Route path="/events" element={<EventList />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
