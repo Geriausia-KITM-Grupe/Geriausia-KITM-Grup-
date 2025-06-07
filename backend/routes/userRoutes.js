f90139d58da4dd6248ba60a07fa5e73947fb01a6
Baltukas <sarunasrusickas09@gmail.com>
2025-06-08 00:55:44 +0300

    Sukurta get-all-users funkcija

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController.js");
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+} = require("../controllers/userController.js");
 
 // Route to register a new user
 // @POST /users/register
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/all", getAllUsers);
 
 module.exports = router;
