679f9c22f53ca9713c8297711ffb87a03d250204
wdallo <web.dalyzz@gmail.com>
2025-06-11 19:59:01 +0300

    Same Cool Stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,35 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import Slider from "./components/silder/Slider";
 import EventList from "./pages/EventList";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 
-import { Routes, Route } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
+  const location = useLocation();
+  const [fadeKey, setFadeKey] = useState(location.pathname);
+
+  useEffect(() => {
+    setFadeKey(location.pathname);
+  }, [location.pathname]);
+
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Slider />
-          </>
-        }
-      />
-      <Route path="/events" element={<EventList />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div key={fadeKey} className="page-fade">
+      <Routes location={location}>
+        <Route path="/" element={<Slider />} />
+        <Route path="/events" element={<EventList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
